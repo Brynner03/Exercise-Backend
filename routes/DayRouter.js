4d6b2462660758ec2fa2328a44510d4b8e6581a5
Brynner Ventura <bventura03@hotmail.com>
2022-05-11 09:39:53 -0400

    editing backend

--- routes/DayRouter.js
+++ routes/DayRouter.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/DayController')
 
 Router.get('/', controller.GetAllDays)
 Router.get('/:id', controller.GetDaysByPk)
-Router.post('/:id', controller.CreateDay)
+Router.post('/create', controller.CreateDay)
 Router.put('/edit/:id', controller.UpdateDay)
 Router.delete('/:id', controller.DestroyDay)
 Router.get('/user/:user_id', controller.GetDayByUser)
